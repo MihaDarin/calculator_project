1be329ef6060c85405629ae827cd2fb6179b84ee
Mike <mihaildaryin@gmail.com>
2021-11-04 13:57:58 +0300

    Edit files

--- main.js
+++ main.js
@@ -1,33 +1,51 @@
-const buttons = document.querySelectorAll('button');
-
+let buttons = document.querySelectorAll('.calc');
+let plusOperator = document.querySelector('.operator');
+plusOperator.addEventListener('click', operation);
+let result = document.getElementById('result');
+let total = document.getElementById('=');
+total.addEventListener('click', totalClick);
+let array = [];
 buttons.forEach((button) =>{
-    button.addEventListener('click', () =>{
+    button.addEventListener('click', () => {
         console.log(button.id);
-    })
-})
+        result.textContent += button.id;
+        array.splice(0, 0, button.id);
+        console.log(array);
+    });
+});
 
-
-function sumNumbers(num1, num2){
-    return num1 + num2
+function summ(array){
+    let summa = array.reduce((prev, curr) => prev + curr, 0);
+    return summa;
 }
+console.log (summ(array));
 
-function minusNumbers(num1, num2){
-    return num1 - num2
+function minus(array){
+    let substract = array.reduce((prev, curr) => prev - curr, 0);
+    return substract
 }
+console.log (minus(array));
 
-function multiplyNumbers(num1, num2){
-    return  num1 * num2
+function multiply(array){
+    let mult = array.reduce((prev, curr) => prev * curr, 1);
+    return mult;
 }
+console.log (multiply(array));
 
-function divideNumbers(num1, num2){
-    return num1 / num2
+function divide(array){
+    let divider = array.reduce((prev,curr) => prev / curr, 1);
+    return divider;
 }
+console.log (divide(array));
 
-function operate(num1, num2){
-    return sumNumbers(num1, num2);
-    return minusNumbers(num1, num2);
-    return multiplyNumbers(num1, num2);
-    return divideNumbers(num1, num2);
-}
-
-// console.log(operate(4, 2));
+function operation(){
+    if(plusOperator.id == "+"){
+       summ(array)
+        console.log(summ(array));
+    }
+};
+function totalClick(){
+    if(total){
+        result.textContent = summ(array);
+    }
+};
